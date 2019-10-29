8ba9cea5d6f5cbfadb2cc10208d07475224f7189
haggagovic <m.haggagovic@gmail.com>
2019-10-28 17:55:23 -0700

    initalize

---
+++ crazytableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crazytableview
+//
+//  Created by mohamed haggag on 10/28/19.
+//  Copyright © 2019 mohamed haggag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
